4472e27fd515598cdc2d777ee1e9a4ac0ece579f
Daisuq <apahomov673@gmail.com>
2025-10-21 11:05:34 +0400

    op

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import rich
 def main():
     rich.print('Hello World from "2025"')
     print('Labuda')
-
-
+    n = int(input())
+    k = 1
+    for i in range(1, n+1, 1):
+        k *= i;
+        print(k)
+    
 if __name__ == "__main__":
     main()
     
